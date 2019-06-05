641f42c2e10273da8b0a0dee162216e39f62d486
Nate Weiskirch <nweiskirch@nweiskirch-macbookpro.roam.corp.google.com>
2019-06-05 16:50:30 -0700

    Price as a string

--- selfie_models.py
+++ selfie_models.py
@@ -4,4 +4,4 @@ class Selfie(ndb.Model):
     title =  ndb.StringProperty(required=True)
     user = ndb.StringProperty(required=True)
     image = ndb.StringProperty(required=True)
-    price = ndb.FloatProperty(default=100.0)
+    price = ndb.StringProperty(default='100.0')
