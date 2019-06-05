9b32ef87c63a26615a3bc9b5f110e1db393d9d9d
Nate Weiskirch <nweiskirch@nweiskirch-macbookpro.roam.corp.google.com>
2019-06-05 16:29:09 -0700

    adding seed data

---
+++ seed_db.py
@@ -0,0 +1,3 @@
+from selfie_models import User, Selfie
+
+Selfie(title='Working Hard', image='https://i.imgur.com/Rpj8BCx.jpg', user='Nate', price=29.99)
--- selfie_models.py
+++ selfie_models.py
@@ -1,10 +1,7 @@
 from google.appengine.ext import ndb
 
-class User(ndb.Model):
-    name =  ndb.StringProperty(required=True)
-
 class Selfie(ndb.Model):
     title =  ndb.StringProperty(required=True)
-    user = ndb.KeyProperty(required=True)
-    image = ndb.BlobProperty(required=True)
+    user = ndb.StringProperty(required=True)
+    image = ndb.StringProperty(required=True)
     price = ndb.FloatProperty(default=100.0)
